f36c6b2284707b45cf72af3bc0abb88e64697b0b
Junjie Gu <youknowgjj@gmail.com>
2019-12-17 20:20:52 -0800

    add bin, allow html parse hard break

--- bin/edit_content.sh
+++ bin/edit_content.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -rf content/post
-git clone git@github.com:SEKIRO-J/BlogNotes.git
-mv BlogNotes content/post
+git clone git@github.com:SEKIRO-J/BlogNotes.git content/post
 
 echo -e "\nnow you can edit your notes, \ndon't forget to push your changes and rebuild"
