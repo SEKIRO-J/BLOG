91415cd66f2eaa0d6bca9fd16e6a490b924329ed
Junjie Gu <youknowgjj@gmail.com>
2019-12-17 19:08:21 -0800

    bin

--- bin/edit_content.sh
+++ bin/edit_content.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -r content
+rm -rf content/post
 git clone git@github.com:SEKIRO-J/BlogNotes.git
-mv BlogNotes content
+mv BlogNotes content/post
 
-echo "now you can edit your notes, don't forget to push your changes and rebuild"
+echo -e "\nnow you can edit your notes, \ndon't forget to push your changes and rebuild"
